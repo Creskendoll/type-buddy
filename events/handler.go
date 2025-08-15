f6a5dc2d4dac7ad70e00ce39358cf356c7178d54
Kenan Soylu <kenan.soylu@doctolib.com>
2025-08-15 20:17:56 +0200

    lil refacto

---
+++ events/handler.go
@@ -0,0 +1,107 @@
+package events
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"time"
+	"unicode"
+
+	"github.com/go-vgo/robotgo"
+	"github.com/ollama/ollama/api"
+	hook "github.com/robotn/gohook"
+
+	"github.com/Creskendoll/type-buddy/llm"
+	debounce "github.com/Creskendoll/type-buddy/timing"
+)
+
+func RunEventLoop(llmClient *api.Client, ctx context.Context, setPredictionText func(string)) {
+	mouseEventChannel := make(chan hook.Event)
+	keyboardEventChannel := make(chan hook.Event)
+
+	// Produce mouse & keyboard events
+	go func() {
+		eventChain := hook.Start()
+		defer hook.End()
+
+		for event := range eventChain {
+			if event.Kind == hook.MouseDown {
+				mouseEventChannel <- event
+			} else if event.Kind == hook.KeyDown || event.Kind == hook.KeyUp {
+				keyboardEventChannel <- event
+			}
+		}
+	}()
+
+	// Consume keyboard events
+	go func() {
+		textBuffer := ""
+		prediction := ""
+		keysToAccept := []uint16{65507, 65507}
+		keysToAcceptState := slices.Clone(keysToAccept)
+
+		debounced := debounce.New(300 * time.Millisecond)
+
+		for event := range keyboardEventChannel {
+			fmt.Println(event)
+
+			if event.Kind == hook.KeyDown {
+				keychar := hook.RawcodetoKeychar(event.Rawcode)
+
+				// Reset the accept shortcut state
+				if len(keysToAcceptState) != len(keysToAccept) && !slices.Contains(keysToAccept, event.Rawcode) {
+					keysToAcceptState = slices.Clone(keysToAccept)
+				}
+
+				// Backspace
+				if event.Rawcode == 65288 && len(textBuffer) > 0 {
+					textBuffer = textBuffer[:len(textBuffer)-1]
+				}
+
+				if !unicode.IsPrint(rune(keychar[0])) && event.Rawcode != 65288 {
+					continue
+				}
+
+				textBuffer += keychar
+
+				if len(textBuffer) > 100 {
+					textBuffer = ""
+				}
+
+				debounced(func() {
+					newPrediction, err := llm.Predict(llmClient, ctx, textBuffer)
+					if err != nil {
+						fmt.Println("Error getting prediction:", err)
+						return
+					}
+
+					if newPrediction == "{KO}" {
+						return
+					}
+
+					setPredictionText(newPrediction)
+					prediction = newPrediction
+				})
+			} else if event.Kind == hook.KeyUp {
+				// If the keysToAcceptState contain the keycode, remove it from the state
+				if slices.Contains(keysToAcceptState, event.Rawcode) {
+					indexToRemove := slices.Index(keysToAcceptState, event.Rawcode)
+					keysToAcceptState = slices.Delete(keysToAcceptState, indexToRemove, indexToRemove+1)
+				}
+
+				if len(keysToAcceptState) == 0 {
+					fmt.Println("Accepting prediction:", prediction)
+					robotgo.TypeStrDelay(prediction, 1000)
+					keysToAcceptState = slices.Clone(keysToAccept)
+				}
+			}
+		}
+	}()
+
+	// Consume mouse events
+	go func() {
+		for mouseEvent := range mouseEventChannel {
+			fmt.Println(mouseEvent)
+		}
+	}()
+}
--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"slices"
-	"time"
-	"unicode"
 
+	"github.com/Creskendoll/type-buddy/events"
 	"github.com/Creskendoll/type-buddy/llm"
-	debounce "github.com/Creskendoll/type-buddy/timing"
-	"github.com/go-vgo/robotgo"
-	hook "github.com/robotn/gohook"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -35,98 +30,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	mouseEventChannel := make(chan hook.Event)
-	keyboardEventChannel := make(chan hook.Event)
+	setPredictionText := func(text string) {
+		fyne.Do(func() {
+			predictionText.SetText(text)
+		})
+	}
 
-	// Produce mouse & keyboard events
-	go func() {
-		eventChain := hook.Start()
-		defer hook.End()
-
-		for event := range eventChain {
-			if event.Kind == hook.MouseDown {
-				mouseEventChannel <- event
-			} else if event.Kind == hook.KeyDown || event.Kind == hook.KeyUp {
-				keyboardEventChannel <- event
-			}
-		}
-	}()
-
-	// Consume keyboard events
-	go func() {
-		textBuffer := ""
-		prediction := ""
-		keysToAccept := []uint16{65507, 65507}
-		keysToAcceptState := slices.Clone(keysToAccept)
-
-		debounced := debounce.New(500 * time.Millisecond)
-
-		for event := range keyboardEventChannel {
-			fmt.Println(event)
-
-			if event.Kind == hook.KeyDown {
-				keychar := hook.RawcodetoKeychar(event.Rawcode)
-
-				// Reset the accept shortcut state
-				if len(keysToAcceptState) != len(keysToAccept) && !slices.Contains(keysToAccept, event.Rawcode) {
-					keysToAcceptState = slices.Clone(keysToAccept)
-				}
-
-				// Backspace
-				if event.Rawcode == 65288 && len(textBuffer) > 0 {
-					textBuffer = textBuffer[:len(textBuffer)-1]
-					continue
-				}
-
-				if !unicode.IsPrint(rune(keychar[0])) {
-					continue
-				}
-
-				textBuffer += keychar
-
-				if len(textBuffer) > 100 {
-					textBuffer = ""
-				}
-
-				debounced(func() {
-					newPrediction, err := llm.Predict(llmClient, ctx, textBuffer)
-					if err != nil {
-						fmt.Println("Error getting prediction:", err)
-						return
-					}
-
-					if newPrediction == "{KO}" {
-						return
-					}
-
-					prediction = newPrediction
-					fyne.Do(func() {
-						predictionText.SetText(prediction)
-					})
-				})
-			} else if event.Kind == hook.KeyUp {
-				// If the keysToAcceptState contain the keycode, remove it from the state
-				if slices.Contains(keysToAcceptState, event.Rawcode) {
-					indexToRemove := slices.Index(keysToAcceptState, event.Rawcode)
-					keysToAcceptState = slices.Delete(keysToAcceptState, indexToRemove, indexToRemove+1)
-				}
-
-				if len(keysToAcceptState) == 0 {
-					fmt.Println("Accepting prediction:", prediction)
-					robotgo.TypeStrDelay(prediction, 1000)
-					keysToAcceptState = slices.Clone(keysToAccept)
-				}
-			}
-
-		}
-	}()
-
-	// Consume mouse events
-	go func() {
-		for mouseEvent := range mouseEventChannel {
-			fmt.Println(mouseEvent)
-		}
-	}()
+	events.RunEventLoop(llmClient, ctx, setPredictionText)
 
 	w.ShowAndRun()
 }
